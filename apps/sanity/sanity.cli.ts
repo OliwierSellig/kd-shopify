04964f568ff8d2898692425b18eafd7f4e2f6971
OliwierSellig <oliwierandrzej.sellig@gmail.com>
2024-11-04 08:18:46 +0100

    initial commit

---
+++ apps/sanity/sanity.cli.ts
@@ -0,0 +1,9 @@
+import { defineCliConfig } from 'sanity/cli'
+
+export default defineCliConfig({
+  api: {
+    projectId: 'xx93h9mw',
+    dataset: 'production',
+  },
+  studioHost: 'kryptonum',
+})
---
+++ apps/sanity/sanity.config.ts
@@ -0,0 +1,31 @@
+import { visionTool } from '@sanity/vision'
+import { defineConfig } from 'sanity'
+import { media } from 'sanity-plugin-media'
+import { structureTool } from 'sanity/structure'
+import { structure } from './structure'
+import { schemaTypes, singletonActions, singletonTypes } from './structure/schema-types'
+import { showProductionUrl } from './utils/show-production-url'
+
+const SANITY_PROJECT_ID = process.env.SANITY_PROJECT_ID!
+
+export default defineConfig({
+  name: 'default',
+  title: 'kryptonum-eu',
+
+  projectId: 'xx93h9mw',
+  dataset: 'production',
+
+  plugins: [structureTool({ structure }), media(), visionTool(), showProductionUrl()],
+
+  schema: {
+    types: schemaTypes,
+    templates: (templates) => templates.filter(({ schemaType }) => !singletonTypes.has(schemaType)),
+  },
+
+  document: {
+    actions: (input, context) =>
+      singletonTypes.has(context.schemaType)
+        ? input.filter(({ action }) => action && singletonActions.has(action))
+        : input,
+  },
+})
